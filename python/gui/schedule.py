1f14e9f5f717a3c3455deaf84cfcd032b4d6c1d7
Dmitry Grigorevich <mitya_107@icloud.com>
2026-08-27 16:18:57 +0300

    schedule полностью готов

--- python/gui/schedule.py
+++ python/gui/schedule.py
@@ -21,7 +21,7 @@ def show_schedule(content_frame):
         tk.Button(
             content_frame,
             text=text,
-            comm=comm
+            command=comm
         ).pack(pady=2)
 
 
@@ -92,39 +92,127 @@ def schedule_day(content_frame):
 
 
 def get_week(start, end):
-    return
+    with connect_db() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("""
+                SELECT s.date_slot, s.time_slot, p.first_name, s.is_free
+                FROM schedule s
+                LEFT JOIN lessons l 
+                    ON s.lesson_id = l.id
+                LEFT JOIN patients_children p
+                    ON p.id = l.patient_id
+                WHERE s.date_slot BETWEEN %s AND %s
+                ORDER BY s.date_slot, s.time_slot ASC
+            """, (start, end))
+            return cursor.fetchall()
 
 def schedule_week(content_frame):
     clear(content_frame)
 
     tk.Label(
         content_frame,
-        text='Укажите дни недели через запятую: (ГГГГ-ММ-ДД, ГГГГ-ММ-ДД)'
+        text='Укажите дни недели:'
     ).grid(column=0, row=0, columnspan=6)
 
-    entry_week = tk.Entry(
-        content_frame,
-        width=30
+
+    tk.Label(
+        content_frame, 
+        text='Начало (ГГГГ-ММ-ДД)'
+    ).grid(column=0, row=1, sticky='e', padx=5)
+
+    entry_start = tk.Entry(
+        content_frame, 
+        width=20
     )
-    entry_week.grid(column=0, row=1, columnspan=6)
+    entry_start.grid(column=1, row=1, pady=5)
 
-    def save_week():
-        week = entry_week.get()
-        if week:
-            data_start, data_end = week.split(', ')
-        else:
-            data_start, data_end = None, None
+    tk.Label(
+        content_frame, 
+        text='Конец (ГГГГ-ММ-ДД)'
+    ).grid(column=0, row=2, sticky='e', padx=5)
 
-        data = get_week(data_start, data_end)
+    entry_end = tk.Entry(
+        content_frame, 
+        width=20
+    )
+    entry_end.grid(column=1, row=2, pady=5)
 
+    def show_week():
+        data = get_week(entry_start.get() or None, entry_end.get() or None)
+        columns = ('Дата', 'Время', 'Имя ребенка', 'Статус')
+        tree = ttk.Treeview(
+            content_frame,
+            columns=columns,
+            show='headings'
+        )
 
+        for col in columns:
+            tree.heading(col, text=col)
+            tree.column(col, width=120)
+
+        for row in data:
+            tree.insert('', tk.END, values=row)
+
+        tree.grid(column=0, row=4, columnspan=6, sticky='nsew')
 
     tk.Button(
         content_frame,
         text='Выбрать неделю',
-        command=save_week
-    )
+        command=show_week
+    ).grid(column=0, row=3)
 
-
+def get_free(start, end):
+    with connect_db() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("""
+                SELECT s.date_slot, s.time_slot
+                FROM schedule s
+                WHERE s.is_free = true
+                AND s.date_slot BETWEEN %s AND %s
+                ORDER BY s.date_slot, s.time_slot ASC
+            """, (start, end))
+            return cursor.fetchall()  
+          
 def schedule_free(content_frame):
-    return
+    clear(content_frame)
+
+    tk.Label(
+        content_frame,
+        text='Свободные места:'
+    ).grid(column=0, row=0, columnspan=6)
+
+    tk.Label(
+        content_frame,
+        text='Начало (ГГГГ-ММ-ДД)'
+    ).grid(column=0, row=1, sticky='e', padx=5)
+    entry_start = tk.Entry(content_frame, width=20)
+    entry_start.grid(column=1, row=1, pady=5)
+
+    tk.Label(
+        content_frame,
+        text='Конец (ГГГГ-ММ-ДД)'
+    ).grid(column=0, row=2, sticky='e', padx=5)
+    entry_end = tk.Entry(content_frame, width=20)
+    entry_end.grid(column=1, row=2, pady=5)
+
+    def show_free():
+        data = get_free(entry_start.get() or None, entry_end.get() or None)
+
+        columns = ('Дата', 'Время')
+        tree = ttk.Treeview(content_frame, columns=columns, show='headings')
+
+        for col in columns:
+            tree.heading(col, text=col)
+            tree.column(col, width=150)
+
+        for row in data:
+            tree.insert('', tk.END, values=row)
+
+        tree.grid(column=0, row=4, columnspan=6, sticky='nsew')
+
+    tk.Button(
+        content_frame,
+        text='Показать',
+        command=show_free
+    ).grid(column=0, row=3, columnspan=6, pady=5)
+
